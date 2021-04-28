505aa4b3f4abe475a87dacead0122f565c70a976
Phil Swatton <philswatton@gmail.com>
2021-04-29 00:27:04 +0100

    users can now specify which descriptive stats they want and in which order

--- R/ordTable.R
+++ R/ordTable.R
@@ -2,13 +2,14 @@
 #'
 #' @param data Dataframe containing the desired variables.
 #' @param vars Character or numeric vector containing integers naming or indexing the columns containing the ordinal variables you wish to summarise.
+#' @param compute Character vector instructing ordTable which descriptive statistics to calculate
 #' @param values NOT YET IMPLEMENTED
 #'
 #' @return A dataframe containing the median, Van der Ejik's agreement statistic, min, 25th quantile, 75th quantile, max, N.
 #' @export
 #'
 #' @examples
-ordTable <- function(data, vars, values=NULL) {
+ordTable <- function(data, vars, compute=c("median", "agreement", "min", "25q", "75q", "max", "N"), values=NULL) {
 
   # Function for producing descriptive stat tables of ordinal data
 
@@ -19,26 +20,71 @@ ordTable <- function(data, vars, values=NULL) {
   # - Implement the 'values' option to allow a list of NULLs & ranges to be given
 
   # TODO (behaviour):
-  # - Allow the user to specify which columns to report <- this will probably take the most work
   # - Consider implementing other measures of ordinal dispersion in the package to allow the user to choose which to report
   # - Search for other ordinal-specific measures
 
 
 
+
+
+
+  # Generate dataframe function conditional on compute vector
+  makeRow <- function(x, name) {
+
+    row <- data.frame(name = name)
+
+    if ("median" %in% compute) {
+      row$median <- stats::median(x, na.rm=T)
+    }
+
+    if ("agreement" %in% compute) {
+      row$agreement <- agreement(x)
+    }
+
+    if ("min" %in% compute) {
+      row$min <- min(vec, na.rm=T)
+    }
+
+    if ("25q" %in% compute) {
+      row$`25q` = stats::quantile(vec, probs=c(.25), na.rm=T)
+    }
+
+    if ("75q" %in% compute) {
+      row$`75q` = stats::quantile(vec, probs=c(.75), na.rm=T)
+    }
+
+    if ("max" %in% compute) {
+      row$max <- max(vec, na.rm=T)
+    }
+
+    if ("N" %in% compute) {
+      row$N <- sum(!is.na(x))
+    }
+
+    # Put into order specified by the user
+    row <- row[c("name",compute)]
+
+    return(row)
+  }
+
+
+
   # Initialise for first variable
   vec <- data[vars[1]][[1]]
-  out <- data.frame(name = vars[1], stats::median(vec, na.rm=T), agreement(vec), min(vec, na.rm=T), stats::quantile(vec, probs=c(.25), na.rm=T), stats::quantile(vec, probs=c(.75), na.rm=T), max(vec, na.rm=T), sum(!is.na(vec)))
+  # out <- data.frame(name = vars[1], stats::median(vec, na.rm=T), agreement(vec), min(vec, na.rm=T), stats::quantile(vec, probs=c(.25), na.rm=T), stats::quantile(vec, probs=c(.75), na.rm=T), max(vec, na.rm=T), sum(!is.na(vec)))
+  out <- makeRow(vec, vars[1])
+
 
   # Keep adding more for subsequent variables if they've been listed
   if (length(vars) > 1) {
     for (i in 2:length(vars)) {
       vec <- data[vars[i]][[1]]
-      out <- rbind(out, data.frame(name = vars[i], stats::median(vec, na.rm=T), agreement(vec), min(vec, na.rm=T), stats::quantile(vec, probs=c(.25), na.rm=T), stats::quantile(vec, probs=c(.75), na.rm=T), max(vec, na.rm=T), sum(!is.na(vec))))
+      # out <- rbind(out, data.frame(name = vars[i], stats::median(vec, na.rm=T), agreement(vec), min(vec, na.rm=T), stats::quantile(vec, probs=c(.25), na.rm=T), stats::quantile(vec, probs=c(.75), na.rm=T), max(vec, na.rm=T), sum(!is.na(vec))))
+      out <- rbind(out, makeRow(vec, vars[i]))
     }
   }
 
-  names(out) <- c("names", "median", "agreement", "min", "25q", "75q", "max", "N")
-  rownames(out) <- NULL #need to work out why it's adding them - probably due to use of quantile() and subsetting
+  # Return the output
   return(out)
 
 }
