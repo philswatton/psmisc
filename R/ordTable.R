fea8b57b22b80430ba3aa26396ff7ccffd12304c
Phil Swatton <philswatton@gmail.com>
2021-04-28 16:09:44 +0100

    to-do list created

--- R/ordTable.R
+++ R/ordTable.R
@@ -2,6 +2,17 @@ ordTable <- function(data, vars, values=NULL) {
 
   # Function for producing descriptive stat tables of ordinal data
 
+  # TODO (inputs):
+  # - Allow 'vars' to be NULL and simply taking a complete df as input
+  # - Input validation (data is df or matrix, vars is an atomic vector of characters or integers, etc)
+  #   - Possibly give option for user to manually specify names - will be important if matrices are taken as input
+  # - Implement the 'values' option to allow a list of NULLs & ranges to be given
+
+  # TODO (behaviour):
+  # - Allow the user to specify which columns to report <- this will probably take the most work
+  # - Consider implementing other measures of ordinal dispersion in the package to allow the user to choose which to report
+  # - Search for other ordinal-specific measures
+
   # Initialise for first variable
   vec <- data[vars[1]][[1]]
   out <- data.frame(name = vars[1], stats::median(vec, na.rm=T), agreement(vec), min(vec, na.rm=T), stats::quantile(vec, probs=c(.25), na.rm=T), stats::quantile(vec, probs=c(.75), na.rm=T), max(vec, na.rm=T), sum(!is.na(vec)))
